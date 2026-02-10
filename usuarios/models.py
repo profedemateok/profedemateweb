60d20a38d6225fd075b026e49356d46777c951fc
Fernando Muiños <fer.muinos@profedemateok.com>
2026-02-10 15:03:34 -0300

    Creación de App usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Usuario(AbstractUser):
+    es_estudiante = models.BooleanField(default=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.username} ({'Estudiante' if self.es_estudiante else 'Admin'})'
