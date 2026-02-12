4fad37d9b5e13c139fba6aa543dfbf793ec1f039
Fernando Muiños <fer.muinos@profedemateok.com>
2026-02-12 09:35:58 -0300

    Views y Templates de Cursos y Clases

--- cursos/urls.py
+++ cursos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_cursos, name='lista_cursos')
+    path('', views.lista_cursos, name='lista_cursos'),
     path('<slug:slug>/', views.detalle_curso, name='detalle_curso')
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Curso, CursoClase
 
 # Create your views here.
+def lista_cursos(request):
+    cursos = Curso.objects.filter(publicado=True)
+    return render(request, 'cursos/lista_cursos.html', {'cursos': cursos})
+
+def detalle_curso(request, slug):
+    curso = get_object_or_404(Curso, slug=slug)
+    clases_ordenadas = CursoClase.objects.filter(curso=curso).order_by('orden').select_related('clase')
+
+    return render(request, 'cursos/detalle_curso.html', {
+        'curso': curso,
+        'clases_ordenadas': clases_ordenadas
+    })
